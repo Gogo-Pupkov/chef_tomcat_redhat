1d69e2517dce20b9d3182c6f55a43424bad9ec1e
Gogo-Pupkov <gogo.pupkov@gmail.com>
2018-06-20 15:43:03 -0400

    added user and group creation

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,4 +4,19 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
+user "tomcat" do
+  manage_home false
+  home '/etc/tomcat'
+  shell "/bin/nologin"
+  comment "Created by Chef"
+  system true
+  provider Chef::Provider::User::Useradd
+  action :create
+end
+
+group "tomcat" do
+  action :create
+  members 'tomcat'
+  append true
+end
 
