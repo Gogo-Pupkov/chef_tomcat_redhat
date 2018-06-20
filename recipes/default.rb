50869d03efea18d836a2cd0ee4f4646bd8f92e8c
Gogo-Pupkov <gogo.pupkov@gmail.com>
2018-06-20 16:30:46 -0400

    download apache tomcat to /tmp, create /opt/tomcat directory, extract it into /opt/tomcat

--- recipes/default.rb
+++ recipes/default.rb
@@ -20,3 +20,30 @@ group "tomcat" do
   append true
 end
 
+#bash 'install_apache_tomcat' do
+#  user 'root'
+#  cwd '/tmp'
+#  code <<-EOH
+#  mkdir /opt/tomcat
+#  wget https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.8/bin/apache-tomcat-9.0.8.tar.gz
+#  tar -zxf apache-tomcat-9*tar.gz -C /opt/tomcat --strip-components=1
+#  EOH
+#end
+#
+
+remote_file '/tmp/apache-tomcat-9.0.8.tar.gz' do
+  source 'https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.8/bin/apache-tomcat-9.0.8.tar.gz'
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create_if_missing
+end
+
+bash 'extract_apache_tomcat' do
+  cwd ::File.dirname('/tmp/apache-tomcat-9.0.8.tar.gz')
+  code <<-EOH
+    mkdir -p /opt/tomcat
+    tar xvf apache-tomcat-9*tar.gz -C /opt/tomcat --strip-components=1
+    EOH
+  not_if { ::File.exist?('/opt/tomcat/RELEASE-NOTES') }
+end
